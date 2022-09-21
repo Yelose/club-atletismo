8aa47d7c4d3ffdb4e89c9675467c01e12bbc32bd
Laura <lauragarciacastelao@hotmail.com>
2022-09-21 13:35:44 +0200

    Finish banner

---
+++ resources/views/components/principal.blade.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="d-flex justify-content-center">
+        <img
+          class="img-fluid full-screen"
+          src="https://media.mybnbwebsite.com/4081/upcoming_events_sunset_03_rs.jpg"
+          alt="Imagen de grupo de superwoman,catwoman, wonderwoman, poison ivy"
+        />
+      </div>
+    
+
+  
+
+
+
+
+</body>
+</html>
+  
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  height: 40vh;
+  margin-top: 3%;
+  margin-bottom: 3s%;
+}
+</style>
