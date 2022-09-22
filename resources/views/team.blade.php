f4e5494bc7a8bf3a79b738e151399fee5d177fed
Iria <iriargon94@gmail.com>
2022-09-22 15:12:04 +0200

    hover foto equipo

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,9 +1,36 @@
 @extends('layouts.main')
 @section("title", "Equipo")
 @section("content")
-<h1>¡Nos presentamos! ¡Somos el Club de Atletismo de Villaviciosa!</h1>
-<section>
-    <img src="https://static3.elnortedecastilla.es/www/multimedia/202009/05/media/equipocontrofeo.jpg" alt="">
+<style>
+    #athletes {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #foto-equipo img{
+        width: 100%;
+        width: 60vw;
+        height: 25.5vw;
+        padding: 2px;
+        border-radius: 5px;
+    }
+
+    #foto-equipo img:hover {
+        transition: all 0.5s ease-in-out;
+        -webkit-transform: scale(1.3);
+        transform: scale(1.3);
+    }
+</style>
+</style>
+
+<section id='athletes'>
+    <h1>¡Nos presentamos! ¡Somos el Club de Atletismo de Villaviciosa!</h1>
+    <div id='foto-equipo'>
+        <img src="https://static3.elnortedecastilla.es/www/multimedia/202009/05/media/equipocontrofeo.jpg" alt="">
+    </div>
+    <h2>Atletas</h2>
 </section>
-<h2>Atletas</h2>
 @endsection
