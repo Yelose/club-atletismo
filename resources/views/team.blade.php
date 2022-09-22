c92a83bed7946145adea5153bb852b9ccc41c38f
Iria <iriargon94@gmail.com>
2022-09-22 14:55:14 +0200

    prueba foto atletas

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 @section("title", "Equipo")
 @section("content")
-<h1>Conoce al equipo</h1>
+<h1>¡Nos presentamos! ¡Somos el Club de Atletismo de Villaviciosa!</h1>
+<section>
+    <img src="https://static3.elnortedecastilla.es/www/multimedia/202009/05/media/equipocontrofeo.jpg" alt="">
+</section>
+<h2>Atletas</h2>
 @endsection
