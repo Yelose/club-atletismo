7d545f3af3749887f7dafcde836ad34115e061bd
Iria <iriargon94@gmail.com>
2022-09-22 14:00:40 +0200

    carpeta images a public

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -60,9 +60,9 @@
 <section id='footer'>
     <h2 id='title-footer'>Club de Atletismo de Villaviciosa</h2>
     <div id='other-links'>
-        <a href="http://fasatle.org/faa/calendar/"><img src="https://scontent.fbio3-1.fna.fbcdn.net/v/t39.30808-6/295051103_541072890893480_8364037363715391393_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=WxyVIGXBBXUAX9HEKuf&_nc_ht=scontent.fbio3-1.fna&oh=00_AT_5K9VZTdVVpM0e93wTd0RRH8LHAsiD2S0gR_7MZH0W4A&oe=632F03DF" alt=""></a>
-        <a href="http://fasatle.org/faa/calendar/"><img src="https://cdn-icons-png.flaticon.com/512/906/906330.png" alt=""></a>
-        <a href="https://www.rfea.es/"><img src="https://habber.com/wp-content/uploads/2022/08/Imagen-WEB-clientes-500x500-1.jpg" alt=""></a>
+        <a href="http://fasatle.org/faa/calendar/"><img src="https://scontent-mad1-2.xx.fbcdn.net/v/t39.30808-6/295051103_541072890893480_8364037363715391393_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=WxyVIGXBBXUAX-v_0PZ&_nc_ht=scontent-mad1-2.xx&oh=00_AT_qmcIoFIBsjc_Fz5m0l8T7GK5u7SNrgD7LzXXMWCQ6aQ&oe=6330FE1F" alt=""></a>
+        <a href="http://fasatle.org/faa/calendar/"><img src="https://cdn-icons-png.flaticon.com/512/906/906330.png" alt="Logo calendario"></a>
+        <a href="https://www.rfea.es/"><img src="https://brandemia.org/sites/default/files/inline/images/rfea_logo.jpg" alt=""></a>
     </div>
     <div id='fem-cod'>
         <p>Marina - Laura - Marybell - Iria - Verónica</p>
