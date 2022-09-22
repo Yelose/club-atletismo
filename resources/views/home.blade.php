983f1f9e5bd28253cc4b6572541b33e59fbdeec0
Marina González Suárez <yelose85@gmail.com>
2022-09-22 20:28:55 +0200

    divided principal component in two components to be able to separate aside in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.main')
 @section("title", "Inicio")
 @section("content")
-<x-principal></x-principal>
+<div class="principal-container">
+    <x-home.slider />
+    <main>
+        <aside>
+            @foreach ($noticias as $noticia)
+            <x-noticias.aside :noticia="$noticia" />
+            @endforeach
+        </aside>
+        <div>
+            <section id="text">
+                <x-home.intro />
+            </section>
+            <section id="testimonials">
+                <x-home.testimonials />
+            </section>
+        </div>
+    </main>
+</div>
 @endsection
