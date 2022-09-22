1957ce2b66fb846cf19c6c5daff9aceeb0922ef3
Marina González Suárez <yelose85@gmail.com>
2022-09-22 09:40:48 +0200

    formatted document

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,36 +1,36 @@
 <style>
-    #nav-menu{
+    #nav-menu {
         background-color: #4D2756;
         grid-template-columns: 20% 80%;
         padding: 10px;
         display: flex;
         align-items: center;
-      
 
-        
+
+
     }
 
-    img.logo-img{
+    img.logo-img {
         width: 4vw;
         object-fit: contain;
         border-radius: 50%;
         align-items: center;
     }
 
-    .buttons a{
+    .buttons a {
         color: white;
         display: flex;
         align-items: center;
         text-decoration: none;
-        font-size: clamp(15px,1.6vw,20px);
-        font-family:Arial, Helvetica, sans-serif;
-        
+        font-size: clamp(15px, 1.6vw, 20px);
+        font-family: Arial, Helvetica, sans-serif;
+
 
     }
 
-   
 
- .buttons{
+
+    .buttons {
         width: 100%;
         height: 30%;
         background: 4D2756;
@@ -38,25 +38,25 @@
         gap: 2vw;
         justify-content: center;
         margin-right: 4vw;
-            }
+    }
 </style>
 
 
-    <section id="nav-menu"> 
+<section id="nav-menu">
 
-        <div id="logo">
+    <div id="logo">
         <img class="logo-img" src="https://st2.depositphotos.com/1012338/6938/v/600/depositphotos_69381837-stock-illustration-running-shield.jpg" alt="logo club de atletismo">
     </div>
 
     <div class="buttons">
-        <a href="{{ url('/') }}" class="active" >HOME</a>
+        <a href="{{ url('/') }}" class="active">HOME</a>
         <a href="{{ url('/history') }}">HISTORIA</a>
-        <a href="{{ url('/team') }}">CLUB</a>
+        <a href="{{ url('/team') }}">EQUIPO</a>
         <a href="{{ url('/news') }}">NOTICIAS</a>
         <a href="{{ url('/achievements') }}">LOGROS</a>
         <a href="{{ url('/calendar') }}">CALENDARIO</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"></i>
+            <i class="fa fa-bars"></i>
         </a>
     </div>
 </section>
