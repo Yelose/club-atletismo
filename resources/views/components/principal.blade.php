b6b3618928b35a06fc25eb42c756db1db77f6310
Marina González Suárez <yelose85@gmail.com>
2022-09-22 15:04:55 +0200

    added aside

--- resources/views/components/principal.blade.php
+++ resources/views/components/principal.blade.php
@@ -1,24 +1,27 @@
-<body>
+<div class="principal-container">
 
-    <div class="d-flex justify-content-center home-banner">
-        <img class="img-fluid full-screen" src="https://media.mybnbwebsite.com/4081/upcoming_events_sunset_03_rs.jpg" alt="Imagen de grupo de superwoman,catwoman, wonderwoman, poison ivy" />
+    <div class="home-banner">
+        <img src="https://media.mybnbwebsite.com/4081/upcoming_events_sunset_03_rs.jpg" alt="Imagen de grupo de superwoman,catwoman, wonderwoman, poison ivy" />
     </div>
+    <main>
+        <aside>
 
-    <section id="text">
+        </aside>
+        <section id="text">
 
-        <div class="text">
-            <h3>Lorem</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aut quia quo eaque. Rerum modi saepe
-                repellendus veritatis? Officia aliquam perferendis tempore doloribus rem delectus veritatis nostrum magnam
-                dolorem reprehenderit!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum error voluptate temporibus amet, ipsa tempore
-                cumque iusto, maiores cupiditate dolore cum corrupti perspiciatis adipisci accusantium atque eligendi nam rerum
-                reiciendis.</p>
-        </div>
-        <div class="contenedor-foto">
-            <img src="https://github.com/lauracastelao/image/blob/main/medallas-definitiva.jpeg?raw=true" alt="nose" id="imagen">
-        </div>
+            <div class="text">
+                <h3>Lorem</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aut quia quo eaque. Rerum modi saepe
+                    repellendus veritatis? Officia aliquam perferendis tempore doloribus rem delectus veritatis nostrum magnam
+                    dolorem reprehenderit!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum error voluptate temporibus amet, ipsa tempore
+                    cumque iusto, maiores cupiditate dolore cum corrupti perspiciatis adipisci accusantium atque eligendi nam rerum
+                    reiciendis.</p>
+            </div>
+            <div class="contenedor-foto">
+                <img src="https://github.com/lauracastelao/image/blob/main/medallas-definitiva.jpeg?raw=true" alt="nose" id="imagen">
+            </div>
 
-    </section>
-
-</body>
+        </section>
+    </main>>
+</div>
