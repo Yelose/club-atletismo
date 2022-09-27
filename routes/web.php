d8279f3ce3327d3eeffde9b6f4f5ea47e3952ac5
lauracastelao <lauragarciacastelao@hotmail.com>
2022-09-27 10:06:19 +0200

    Calendar works fine

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\TeamController;
+use App\Http\Controllers\FullCalendarController;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/calendar', [CalendarController::class, 'calendar'])->name('calendar');
@@ -14,6 +15,8 @@ Route::get('/history', [HistoryController::class, 'history'])->name('history');
 Route::get('/news', [NewsController::class, 'news'])->name('news');
 Route::get('/team', [TeamController::class, 'team'])->name('team');
 Route::get('/achievements', [AchievementsController::class, 'achievements'])->name('achievements');
+Route::get('fullcalendar', [FullCalendarController::class, 'index']);
+Route::post('fullcalendar-ajax', [FullCalendarController::class, 'ajax']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
