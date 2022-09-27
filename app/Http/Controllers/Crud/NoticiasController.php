1d48684e2ec93c2b33cc72b8f98ded1807c6d7f9
Marina González Suárez <yelose85@gmail.com>
2022-09-27 14:58:59 +0200

    unsolved sass merge

--- app/Http/Controllers/Crud/NoticiasController.php
+++ app/Http/Controllers/Crud/NoticiasController.php
@@ -15,21 +15,6 @@ class NoticiasController extends Controller
     public $selected_id, $keyWord, $titular, $piefoto, $imagen, $subtitulo, $noticia, $fecha;
     public $updateMode = false;
 
-    // public function render()
-    // {
-    //     $keyWord = '%' . $this->keyWord . '%';
-    //     return view('noticias.index', [
-    //         'noticias' => noticia::latest()
-    //             ->orWhere('titular', 'LIKE', $keyWord)
-    //             ->orWhere('imagen', 'LIKE', $keyWord)
-    //             ->orWhere('piefoto', 'LIKE', $keyWord)
-    //             ->orWhere('subtitulo', 'LIKE', $keyWord)
-    //             ->orWhere('noticia', 'LIKE', $keyWord)
-    //             ->orWhere('fecha', 'LIKE', $keyWord)
-    //             ->paginate(10),
-    //     ]);
-    // }
-
     public function cancel()
     {
         $this->resetInput();
@@ -104,5 +89,23 @@ class NoticiasController extends Controller
             $record = Noticia::where('id', $id);
             $record->delete();
         }
+        //falta mensaje de error
     }
+
+    // public function render()
+    // {
+    //     $keyWord = '%' . $this->keyWord . '%';
+    //     return view('noticias.index', [
+    //         'noticias' => noticia::latest()
+    //             ->orWhere('titular', 'LIKE', $keyWord)
+    //             ->orWhere('imagen', 'LIKE', $keyWord)
+    //             ->orWhere('piefoto', 'LIKE', $keyWord)
+    //             ->orWhere('subtitulo', 'LIKE', $keyWord)
+    //             ->orWhere('noticia', 'LIKE', $keyWord)
+    //             ->orWhere('fecha', 'LIKE', $keyWord)
+    //             ->paginate(10),
+    //     ]);
+    // }
+
+    // orderBy
 }
