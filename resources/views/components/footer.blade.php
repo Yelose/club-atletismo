8426bd0b0ebac49faac1ce51fef73c28c62608ac
Marina González Suárez <yelose85@gmail.com>
2022-10-06 09:49:01 +0200

    changed background, display items in footer responsive

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,15 +1,21 @@
-<section id='footer'>
+<footer id='footer'>
     <h2 id='title-footer'>Club de Atletismo de Villaviciosa</h2>
     <div class="footer-container">
         <div id='other-links'>
-            @foreach ($links as $link)
-            <a href="{{$link->link}}"><img src="{{$link->image}}" alt="{{$link->name}}"></a>
-            @endforeach
+            <section class="external-links-container">
+                <h4>Enlaces de interés</h4>
+                <div class="links">
+                    @foreach ($links as $link)
+                    <a href="{{$link->link}}"><img src="{{$link->image}}" alt="{{$link->name}}"></a>
+                    @endforeach
+                </div>
+            </section>
         </div>
         <div id='fem-cod'>
             <section>
-                <h4>Página hecha con</h4>
+                <h4>Página hecha por</h4>
                 <img src="https://femcoders.factoriaf5.org/wp-content/uploads/2021/12/Logo-FemCodersF5-color.png" alt="Factoría F5">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/985px-Laravel.svg.png" alt="Laravel">
             </section>
             <section>
                 <p>Marina</p>
@@ -19,5 +25,5 @@
             </section>
         </div>
     </div>
-    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline" id='login'>Log in</a>
-</section>
+    <a href="{{ route('login') }}" class="login" id='login'>Entrar como Administrador</a>
+</footer>
