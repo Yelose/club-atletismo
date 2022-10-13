670dc64e7a3edb78d1aa0d4ad78940ded11acfd7
MBellJDD <marybelljd@gmail.com>
2022-10-13 14:31:27 +0200

    changes in history.blade.php

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -4,14 +4,19 @@
 
 
 <div class="history.container">
-
-<h1>{{ $quiens->titulo}}</h1>
-<hr>
-<h2>{{ $quiens->image}}</h2>
+    <h1>{{ $quiens->titulo}}</h1>
+    <hr>
+    <h2>{{ $quiens->image}}</h2>
 </div>
 <hr>
 <h3>{{ $quiens->resumen}}</h3>
+@endsection
 
+
+@section("content")
+<div class="history.container">
+    <h1>{{ $cronologia->fecha}}</h1>
+    <h3>{{ $cronologia->resumen}}</h3>
 </div>
 @endsection
 
