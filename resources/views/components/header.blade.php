1164b99f88bf0112341571d11aecb997098c4805
Marina González Suárez <yelose85@gmail.com>
2022-10-13 18:06:55 +0200

    responsive menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,9 @@
-<section id="nav-menu">
-
+<nav id="nav-menu">
     <div id="logo">
         <img class="logo-img" src="https://st2.depositphotos.com/1012338/6938/v/600/depositphotos_69381837-stock-illustration-running-shield.jpg" alt="logo club de atletismo">
     </div>
 
-    <h1>Club de Atletismo Villaviciosa</h1>
+    <h1>Club de Atletismo El Gaitero</h1>
 
     <div class="buttons">
         <a href="{{ url('/') }}" class="active">HOME</a>
@@ -13,9 +12,23 @@
         <a href="{{ url('/news') }}">NOTICIAS</a>
         <a href="{{ url('/achievements') }}">LOGROS</a>
         <a href="{{ url('/calendar') }}">CALENDARIO</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>
-        </a>
     </div>
-    <div class="header-right"></div>
-</section>
+    <a class="menu-toggle" onclick="toggle()">
+        <img src="{{asset('assets/img/menu.png')}}" alt="menu">
+    </a>
+</nav>
+<div id="menu" class="hidden">
+    <div class="mobile-menu">
+        <a href="{{ url('/') }}">HOME</a>
+        <a href="{{ url('/history') }}">HISTORIA</a>
+        <a href="{{ url('/team') }}">EQUIPO</a>
+        <a href="{{ url('/news') }}">NOTICIAS</a>
+        <a href="{{ url('/achievements') }}">LOGROS</a>
+        <a href="{{ url('/calendar') }}">CALENDARIO</a>
+    </div>
+</div>
+
+<script>
+    const menu = document.getElementById('menu');
+    const toggle = () => menu.classList.toggle("hidden");
+</script>
