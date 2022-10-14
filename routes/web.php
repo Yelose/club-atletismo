78ddf2fab0a0ed9379c4dd898dbff681fd714d13
Laura <lauragarciacastelao@hotmail.com>
2022-10-14 14:37:27 +0200

    Add route middleware calendar

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,13 @@ use App\Http\Controllers\TeamController;
 use App\Http\Controllers\FullCalendarController;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
-Route::get('/calendar', [CalendarController::class, 'calendar'])->name('calendar');
+Route::get('/calendar', [CalendarController::class, 'calendar'])->name('calendar')->middleware(['auth']);
 Route::get('/history', [HistoryController::class, 'history'])->name('history');
 Route::get('/news', [NewsController::class, 'news'])->name('news');
 Route::get('/team', [TeamController::class, 'team'])->name('team');
 Route::get('/achievements', [AchievementsController::class, 'achievements'])->name('achievements');
 Route::get('fullcalendar', [FullCalendarController::class, 'index']);
 Route::post('fullcalendar-ajax', [FullCalendarController::class, 'ajax']);
-
-Route::get('/noticia/{noticia}', [NewsController::class, 'noticia']);
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
