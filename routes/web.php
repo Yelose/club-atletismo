3941cd0516a1470f5174a5fa7c7aef44d0594d92
Laura <lauragarciacastelao@hotmail.com>
2022-10-18 10:06:08 +0200

    Add route middle

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\TeamController;
 use App\Http\Controllers\FullCalendarController;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
-Route::get('/calendar', [CalendarController::class, 'calendar'])->name('calendar');
+Route::get('/calendar', [CalendarController::class, 'calendar'])->name('calendar')->middleware(['auth']);
 Route::get('/history', [HistoryController::class, 'history'])->name('history');
 Route::get('/news', [NewsController::class, 'news'])->name('news');
 Route::get('/team', [TeamController::class, 'team'])->name('team');
