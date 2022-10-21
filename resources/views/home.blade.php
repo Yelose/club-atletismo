ed07f5e2f5379e1728f0636e6a859ea2f56fe702
Laura <lauragarciacastelao@hotmail.com>
2022-10-21 15:34:04 +0200

    Finish day task work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,6 +34,15 @@
             </section>
         </div>
     </main>
+    {{-- <div class="d-flex justify-content-center mt-5 h-100">
+        <div class="d-flex align-items-center align-self-center card p-3 text-center cookies"><img src="https://i.imgur.com/Tl8ZBUe.png" width="50"><span class="mt-2">We use third party cookies to personalize content, ads and analyze site traffic.</span><a class="d-flex align-items-center" href="#">Learn more<i class="fa fa-angle-right ml-2"></i></a>
+            <button
+                class="btn btn-dark mt-3 px-4" type="button">Okay</button>
+        </div>
+    </div> --}}
+
+   
+
 </div>
 <script>
     var slideIndex = 1;
@@ -62,6 +71,7 @@
         }
         slides[slideIndex - 1].style.display = "block";
     }
+    
 </script>
 
 @endsection
