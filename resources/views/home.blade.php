d8c1f0cc2c3d1cf5a8a589409881ed8c274b744c
Marina González Suárez <yelose85@gmail.com>
2022-10-23 11:17:30 +0200

    home script for testimonials working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,44 +38,29 @@
             </section>
         </div>
     </main>
-    {{-- <div class="d-flex justify-content-center mt-5 h-100">
-        <div class="d-flex align-items-center align-self-center card p-3 text-center cookies"><img
-                src="https://i.imgur.com/Tl8ZBUe.png" width="50"><span class="mt-2">We use third party cookies to
-                personalize content, ads and analyze site traffic.</span><a class="d-flex align-items-center"
-                href="#">Learn more<i class="fa fa-angle-right ml-2"></i></a>
-            <button class="btn btn-dark mt-3 px-4" type="button">Okay</button>
-        </div>
-    </div> --}}
-
-
-
 </div>
 <script>
-
     var slideIndex = 1;
-    showSlides(slndex);
-
+    showSlides(slideIndex);
     function plusSlides(n) {
         showSlides(slideIndex += n);
     }
-
     function currentSlide(n) {
         showSlides(slideIndex = n);
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {
-                slideIndex = 1
-            }
-            if (n < 1) {
-                slideIndex = slides.length
-            }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slides[slideIndex - 1].style.display = "block";
+    }
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        if (n > slides.length) {
+            slideIndex = 1
         }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
     }
 </script>
 
