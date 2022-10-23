438aacf8a1552430aa031dfd7095d4915f690858
Laura <lauragarciacastelao@hotmail.com>
2022-10-23 14:39:43 +0200

    Changue size in container user calendar

--- resources/views/fullcalendar.blade.php
+++ resources/views/fullcalendar.blade.php
@@ -2,6 +2,11 @@
 @section("title", "Calendario")
 @section("content")
 
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
 <div class="container">
     <div class="jumbotron">
         <div class="container text-center">
@@ -27,6 +32,9 @@
 
         </header>
     </div>
+</div>
+            </div>
+        </div>
     <div class="mb-5" id='calendar'>
     </div>
 </div>
