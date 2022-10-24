51bd70c2b29fa9277c34a6ab99223dee3f39afe8
Iria <iriargon94@gmail.com>
2022-10-24 09:39:20 +0200

    estilo editar entrenadores y noticias

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -5,78 +5,61 @@
         </h2>
     </x-slot>
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Editar noticia</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('noticias.index') }}"> Volver</a>
-            </div>
-        </div>
-    </div>
-
     @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Uy!</strong> Hay algún problema con los datos introducidos.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div class="alert alert-danger">
+        <strong>Uy!</strong> Hay algún problema con los datos introducidos.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
     @endif
 
-    <form action="{{ route('noticias.update',$noticia->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
+    <div class="crud-create-edit-container">
+        <h2>Editando {{$noticia->name}}</h2>
+        <img src="/images/{{ $noticia->image }}">
+        <form action="{{ route('noticias.update',$noticia->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
 
-         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Titular:</strong>
-                    <input type="text" name="titular" value="{{ $noticia->titular }}" class="form-control" placeholder="titular">
-                </div>
+            <section class="form-group">
+                <strong>Foto:</strong>
+                <input type="file" name="image" class="form-control" placeholder="image">
+            </section>
+            <div class="form-group">
+                <strong>Titular:</strong>
+                <input type="text" name="titular" value="{{ $noticia->titular }}" class="form-control" placeholder="titular">
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Foto:</strong>
-                    <input type="file" name="image" class="form-control" placeholder="image">
-                    <img src="/images/{{ $noticia->image }}" width="300px">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Pie de foto:</strong>
-                    <input type="text" name="piefoto" value="{{ $noticia->piefoto }}" class="form-control" placeholder="pie de foto">
-                </div>
+            <div class="form-group">
+                <strong>Pie de foto:</strong>
+                <input class="form-control" name="piefoto" placeholder="pie de foto" value="{{ $noticia->piefoto }}"></input>
             </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Subtítulo:</strong>
-                    <input type="text" name="subtitulo" value="{{ $noticia->subtitulo }}" class="form-control" placeholder="subtitulo">
-                </div>
+            <div class="form-group">
+                <strong>Subtítulo:</strong>
+                <input type="text" name="subtitulo" value="{{ $noticia->subtitulo }}" class="form-control"
+                    placeholder="Categoría">
             </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Noticia:</strong>
-                    <input type="text" name="noticia" value="{{ $noticia->noticia }}" class="form-control" placeholder="noticia">
-                </div>
+            <section class="form-group">
+                <strong>Noticia:</strong>
+                <textarea type="text" name="noticia" value="{{ $noticia->noticia }}" class="form-control"
+                    placeholder="noticia"></textarea>
+            </section>
+
+            <div class="form-group">
+                <strong>Fecha:</strong>
+                <input type="text" name="fecha" value="{{ $noticia->fecha }}" class="form-control"
+                    placeholder="fecha">
             </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Fecha:</strong>
-                    <input type="text" name="fecha" value="{{ $noticia->fecha }}" class="form-control" placeholder="fecha">
-                </div>
+            <div class="buttons-group">
+                <button type="submit" class="create-button">Aceptar</button>
+                <a class="cancel-button" href="{{ route('noticias.index') }}">Cancelar</a>
             </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-              <button type="submit" class="btn btn-primary">Aceptar</button>
-            </div>
-        </div>
+        </form>
+    </div>
 
-    </form>
 </x-app-layout>
--- resources/views/admin/trainers/edit.blade.php
+++ resources/views/admin/trainers/edit.blade.php
@@ -5,56 +5,44 @@
         </h2>
     </x-slot>
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Editar entrenador</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('trainers.index') }}"> Volver</a>
-            </div>
-        </div>
-    </div>
-
     @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Uy!</strong> Hay algún problema con los datos introducidos.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div class="alert alert-danger">
+        <strong>Uy!</strong> Hay algún problema con los datos introducidos.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
     @endif
 
-    <form action="{{ route('trainers.update',$trainer->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
+    <div class="crud-create-edit-container">
+        <h2>Editando {{$trainer->name}}</h2>
+        <img src="/images/{{ $trainer->image }}">
+        <form action="{{ route('trainers.update',$trainer->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
 
-         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Nombre:</strong>
-                    <input type="text" name="name" value="{{ $trainer->name }}" class="form-control" placeholder="Name">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Puesto:</strong>
-                    <textarea class="form-control" style="height:150px" name="roll" placeholder="Puesto">{{ $trainer->roll }}</textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Foto:</strong>
-                    <input type="file" name="image" class="form-control" placeholder="foto">
-                    <img src="/images/{{ $trainer->image }}" width="300px">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-              <button type="submit" class="btn btn-primary">Aceptar</button>
-            </div>
-        </div>
+            <section class="form-group">
+                <strong>Foto:</strong>
+                <input type="file" name="image" class="form-control" placeholder="image">
+            </section>
 
-    </form>
+            <div class="form-group">
+                <strong>Nombre:</strong>
+                <input type="text" name="name" value="{{ $trainer->name }}" class="form-control" placeholder="Name">
+            </div>
+
+            <div class="form-group">
+                <strong>Puesto:</strong>
+                <input class="form-control" name="roll" placeholder="puesto" value="{{ $trainer->roll }}"></input>
+            </div>
+
+            <div class="buttons-group">
+                <button type="submit" class="create-button">Aceptar</button>
+                <a class="cancel-button" href="{{ route('trainers.index') }}">Cancelar</a>
+            </div>
+
+        </form>
+    </div>
 </x-app-layout>
